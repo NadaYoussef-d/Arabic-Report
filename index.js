6944a3c99892e90133514e01ef827b190355f8c3
NadaYoussef <yousefnada95@gmail.com>
2020-07-09 21:31:16 +0200

    print2

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function PrintReceipt() {
     : frame1[0].contentDocument;
   frameDoc.document.open();
   //Create a new HTML document.
-  frameDoc.document.write("<html><head>");
+  frameDoc.document.write(`<html dir="rtl" lang="ar"><head>`);
   //Append the external CSS file.
   frameDoc.document.write(`<link href="style.css" rel="stylesheet"/>`);
   frameDoc.document.write("</head><body>");
   frameDoc.document.write(contents);
   frameDoc.document.write("</body></html>");
 
-  frameDoc.document.write("<hr>");
+  frameDoc.document.write(`<br style ="line-height:350px;">`);
   //repeat the content
   frameDoc.document.write("<html><head>");
   //Append the external CSS file.
